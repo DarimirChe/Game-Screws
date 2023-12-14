013c6a0bc5ba906e22e8983a4f52cb98613806e1
DarimirChe <darimrulit@gmail.com>
2023-12-15 00:32:48 +0300

    Коммит с последними изменениями
    Выкручивание болта по нажатию

---
+++ Assets/Scripts/ScrewsSelection.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrewsSelection : MonoBehaviour
+{
+    //[SerializeField] GameObject Screw;
+
+    public bool Twisted = false;
+
+    //обьявляем переменную для луча
+    //   RaycastHit hit;
+    //  Ray MyRay;
+
+    Animator anim;
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+
+
+
+        //if (Input.GetKeyDown(KeyCode.Mouse0))
+        //{
+        //    MyRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    Debug.DrawRay(MyRay.origin, MyRay.direction * 10, Color.yellow);
+        //    if (Physics.Raycast(MyRay, out hit, 100))
+        //    {
+        //        MeshFilter filter = hit.collider.GetComponent(typeof(MeshFilter)) as MeshFilter;
+        //        if (filter)
+        //        {
+        //            //имя обьекта по которому щелкнули мышей               
+        //           // Debug.Log(filter.gameObject.name);
+        //            if (filter.gameObject.name == "hole")
+        //            {
+        //                Debug.Log("OnHole");
+        //                Vector3 scores = filter.gameObject.transform.position;
+        //                Screw.transform.position = scores;
+        //                SelectScrew(true);
+
+        //            }
+        //        }
+        //    }
+        //}
+        void OnMouseDown()
+        {
+
+            Twisted = !Twisted;
+            SelectScrew(Twisted);
+        }
+    
+
+    public void SelectScrew(bool Twisted)
+    {
+        if (Twisted)
+        {
+            anim.SetTrigger("Out");
+            Debug.Log("Untwisted");
+
+        }
+        else
+        {
+            anim.SetTrigger("In");
+            Debug.Log("Twisted");
+
+
+        }
+    }
+
+}
